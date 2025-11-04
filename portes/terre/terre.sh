e273c2815a695455d2db7d86a8f22371088e1f48
AnasSfar <sfaranas13@gmail.com>
2025-11-04 21:51:59 +0100

    Create terre.sh

---
+++ portes/terre/terre.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -euo pipefail
+
+# temps et score
+debut=$(date +%s)
+
+# dossiers
+state_dir=".game_state/terre"
+mkdir -p "$state_dir"
+
+
+echo " POPRTE DE LA TERRE - EPREUVE 4"
+sleep 2
+echo "BiENveNueeeeeeeeeeeeeeeeeee chER joUeUUUr"
+sleep 2
+echo "..."
+echo "Je Sui-" 
+sleep 0.5
+echo "Je SuiS La TeRRRRe- l'homme m'a"
+sleep 0.5
+echo "détruit"
+sleep 0.5
+
+echo "Aide moi"
+sleep 0.5
+
+echo "SOS"
+sleep 0.5
+
+echo "L'homme m'a ren/inversé avec ces actions... trop de CO2... trop de méchaaaaanceté.. "
+
+#effet réel
+for c in "a" "u" "   " "s" "e" "c" "o" "u" "r" "s" "..." ; do
+  echo -n "$c"
+  sleep 0.1
+done
+echo ""
+sleep 5
+
+#début de l'épreuve
+echo ".etiurTéd a’m emmoh’l
+.erreA al sius eJ
+.Revuas em erocne xuep ut
+.eiuofne ia’j euq eéhcac evihcra’l ehcrehc ,alec ruop
+.tneitnoc elle’uq sreihcif sel sil te al-tiar txe
+.elcycer ,esuer ,ecuder :sriopse siort sem tnevuort es euq àl tse’c"
